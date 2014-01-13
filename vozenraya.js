6f837c09f77469842d92771d10a2a87d3eea20e3
Titanio Verde <titanioverde@gmail.com>
2014-01-13 19:54:57 +0100

    Muuuchos comentarios. Los necesitaba.

--- vozenraya.js
+++ vozenraya.js
@@ -38,18 +38,18 @@ var Board = function (Chip) {
 	
 	//An empty board.
 	this.Squares = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]];
-	this.mainQueue = [];
-	this.mainBusy = false;
 	
 	var parentThis = this; //Never forget the main object.
 	
-	//Error control and options.
+	//Errors, control and options.
 	if (window.location.search.indexOf("debug") > -1) {
 		this.debug = true;
 	} else {
 		this.debug = false;
 	}
 	
+	this.mainQueue = [];
+	this.mainBusy = false; //Semaphore, basically.
 	this.microphoneWorks = this.debug;
 	this.failCount = 0;
 	this.micBusy = false;
@@ -101,7 +101,8 @@ var Board = function (Chip) {
 		"play-menu": new Audio(this.VoicePath + "play-menu.ogg")
 	}
 
-	//A generic voice recognition object (despite there's "anotherVoice" further).
+	//A generic voice recognition object (despite there are other objects like this later).
+	//Documentation: https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
 	this.voiceReceiver = new webkitSpeechRecognition();
 	this.voiceReceiver.lang = "es-ES";
 	this.voiceReceiver.onresult = function (event) {
@@ -111,19 +112,20 @@ var Board = function (Chip) {
 		}
 	}
 	
+	//To execute at first if mic wasn't tested before. The user must say three words
 	this.checkMicrophone = function() {
 		this.microphoneWorks = false;
 		var voiceTest = new webkitSpeechRecognition();
 		voiceTest.lang = "es-ES";
+		
 		voiceTest.onresult = function(event) {
 			if (event.results.length > 0) {
 				phrase = event.results[0][0].transcript;
-				results = parentThis.recognizePosition(phrase);
-				console.log(results);
-				if (results.indexOf(-1) > -1) {
+				results = parentThis.recognizePosition(phrase); //User has to pronounce "1, 2, 3", just like board positions.
+				if (results.indexOf(-1) > -1) { //Number not recognized.
 					queue = [parentThis.Voice["check-mic-fail"]];
 					parentThis.failCount += 1;
-				} else {
+				} else { //All clear.
 					parentThis.microphoneWorks = true;
 					queue = [parentThis.Voice["check-mic-ok"],
 							 parentThis.Voice["general-help"],
@@ -132,6 +134,7 @@ var Board = function (Chip) {
 							 parentThis.Voice["start"]];
 				}
 				parentThis.audioQueue(queue, 100, function() { parentThis.micBusy = false; });
+				//Play those voice samples, and free mic when finished.
 			}
 		}
 		
@@ -142,33 +145,34 @@ var Board = function (Chip) {
 			}
 		}
 		
+		//First let's play the instructions, and then do all the action and open the mic.
 		this.audioQueue([this.Voice["check-mic"]], 200, function() {
 			var forTheCheck = setInterval(function() {
 				if (parentThis.microphoneWorks == false) {
 					if (parentThis.micBusy == false) {
-						//console.log("o.O");
-						if (parentThis.failCount >= 3) {
+						if (parentThis.failCount >= 3) { //Too much recognition errors? I quit.
 							parentThis.audioQueue([parentThis.Voice["not-working"]], 1);
 							clearInterval(forTheCheck);
-						} else {
+						} else { //Start!
 							parentThis.micBusy = true;
 							voiceTest.start();
 						}
 					}
-				} else {
+				} else { //It works! Clear and let's get started.
 					parentThis.failCount = 0;
 					clearInterval(forTheCheck);
 				}
 			}, 60);
 		});
-		
 	}
 	
+	//To retrieve the current board as a string.
 	this.showBoard = function () {
 		var output = String(this.Squares[0]) + "\n" + String(this.Squares[1]) + "\n" + String(this.Squares[2]);
 		return (output);
 	}
 	
+	//Adapt the recognized words to board positions.
 	this.recognizePosition = function(sample) {
 		var words = sample.toLowerCase().split(" ");
 		var results = [];
@@ -184,20 +188,24 @@ var Board = function (Chip) {
 		return (results);
 	}
 	
+	//The user said a non-number word. Was it a command?
 	this.compareCommand = function(sample, dict) {
 		var word = sample.toLowerCase().split(" ")[0];
 		var result = "";
 		for (var i in dict) {
 			if (dict[i].toString().indexOf(word) > -1) {
+				//Yes, it is. Here is the array position for that command.
 				result = i;
 			}
 		}
 		return result;
 	}
 	
+	//Command menu between normal game turns.
 	this.commandPlayMenu = function(index) {
 		switch (index) {
 			case "0":
+				//Bring some help.
 				return [this.Voice["available-actions"], this.Voice["play-menu"]];
 				break;
 				
@@ -207,6 +215,7 @@ var Board = function (Chip) {
 				break;
 			
 			case "2":
+				//Instructions. How to put a chip.
 				return [this.Voice["put-help"]];
 				break;
 			
@@ -221,24 +230,29 @@ var Board = function (Chip) {
 		return "void";
 	}
 	
+	//Deprecated?
+	//Our nice friend will tell you which chip is occupying every board square.
 	this.readBoard = function(toQueue) {
 		var queue = [];
 		var Squares = this.Squares;
 		for (var i in Squares) {
 			var currentRow = parseInt(i) + 1;
-			queue.push(this.Voice["row" + String(currentRow)]);
+			queue.push(this.Voice["row" + String(currentRow)]); //Say the current board row.
 			for (var j in Squares[i]) {
-				queue.push(this.Voice[Squares[i][j]]);
+				queue.push(this.Voice[Squares[i][j]]); //Say its color chip, or if it's empty.
 			}
 		}
 		if (toQueue) {
+			//For another functions.
 			return queue;
 		} else {
+			//To play right now.
 			this.audioQueue(queue, 200);
 		}
 		return 0;
 	}
 	
+	//Current board status (chip positions and empty squares) for later voice play.
 	this.queueBoard = function() {
 		var queue = [];
 		var Squares = this.Squares;
@@ -246,19 +260,21 @@ var Board = function (Chip) {
 			var currentRow = parseInt(i) + 1;
 			queue.push("row" + String(currentRow));
 			for (var j in Squares[i]) {
-				queue.push(Squares[i][j]);
+				queue.push(Squares[i][j]); //Its chip color, or if it's empty.
 			}
 		}
-		return queue;
+		return queue; //Just a string array. But they're names appearing in Voice list.
 	}
 	
-	
+	//Main and wonderful function to orderly play audio samples.
+	//It will execute another function when finished (callback) if specified. 
 	this.audioQueue = function(queue, delay, callback) {
 		delay = delay || 500;
 		var i = 0;
 		var playNow = function(audio) {
 			audio.play();
 			i += 1;
+			//Interval to call playNow (yeah, this function) when audio has finished playing.
 			var waitPlease = setInterval(function() {
 				if (i < queue.length) {
 					if (audio.ended) {
@@ -266,10 +282,11 @@ var Board = function (Chip) {
 						clearInterval(waitPlease);
 					}
 				} else {
+					//Execute callback when the queue is done.
 					if (typeof(callback) == "function") {
 						var waitPlease2 = setInterval(function() {
 							if (audio.ended) {
-								//TODO: Is there really no cleaner way to do this?! T_T
+								//Is there really no cleaner way to do this?! T_T
 								setTimeout(function() { callback() }, delay);
 								clearInterval(waitPlease2);
 							}
@@ -279,9 +296,10 @@ var Board = function (Chip) {
 				}
 			}, 50);
 		}
-		playNow(queue[i]);
+		playNow(queue[i]); //Start!
 	}
 	
+	//An unfinished attempt to fit everything in a universal FIFO queue.
 	this.waitForVoice = function(onResult, callback, audioQueue) {
 		
 		
@@ -307,12 +325,14 @@ var Board = function (Chip) {
 		}, 50);
 	}
 	
+	//Board default state.
 	this.emptyBoard = function() {
 		this.Squares = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]];
 		
 		//this.changeTurn();
 	}
 	
+	//To check if there's a horizontal, vertical or diagonal line of the same color.
 	this.theresLine = function() {
 		var Squares = this.Squares;
 		
@@ -354,6 +374,7 @@ var Board = function (Chip) {
 		return 0;
 	}
 	
+	//Run when board is full and no line condition is true.
 	this.theresTie = function () {
 		if (this.showBoard().indexOf("X") == -1) {
 			console.log("Empate. :-D-:");
@@ -362,6 +383,7 @@ var Board = function (Chip) {
 		return 0;
 	}
 	
+	//Only for visual control. To delete soon.
 	this.decideHuman = function () {
 		var Row = prompt(this.Color + ". Fila 1, 2 o 3?");
 		Row -= 1;
@@ -370,36 +392,41 @@ var Board = function (Chip) {
 		return [Row, Column];
 	}
 	
+	//Check if there's a chip on this specific square.
 	this.isOccupied = function (Row, Column) {
 		if (this.Squares[Row][Column] != "X") {
-			return 1;
+			return 1; //It is.
 		} else {
-			return 0;
+			return 0; //It is not.
 		}
 	}
 	
+	//Forms an array with every empty position. Useful for AI.
 	this.listEmptySquares = function () {
 		var empties = [];
 		var Squares = this.Squares;
 		for (var row in Squares) {
 			for (var column in Squares[row]) {
 				if (!(this.isOccupied(row, column))) {
-					empties.push([row, column]);
+					empties.push([row, column]); //Row and column as a sub-array.
 				}
 			}
 		}
 		return empties;
 	}
 	
+	//Basically change an "X" (empty) for a "B" (black) or a "W" (white).
 	this.putChip = function (Chip, Row, Column) {
-		if (!(this.isOccupied(Row, Column))) {
+		if (!(this.isOccupied(Row, Column))) { //Before that, check if it's really empty.
 			this.Squares[Row][Column] = Chip;
 			return 1;
 		} else {
-			return 0;
+			return 0; //Oops! This square is already taken.
 		}
 	}
 	
+	//Alternates between each player for the next turn.
+	//Or if dontChange, just return which would be the next player.
 	this.changeTurn = function (dontChange) {
 		dontChange = dontChange || false;
 		var otherChip, otherColor;
@@ -420,6 +447,7 @@ var Board = function (Chip) {
 		return [otherChip, otherColor];
 	}
 	
+	//Futurely deleted. For visual playing.
 	this.basicTurnFlow = function(Target) {
 		if (!(this.isOccupied(Target[0], Target[1]))) {
 			this.putChip(this.Chip, Target[0], Target[1]);
@@ -432,40 +460,41 @@ var Board = function (Chip) {
 		}
 	}
 	
+	//The core of a turn. Put the chip if the square is free, check if there's a line, and next turn (or not).
 	this.basicTurnFlowWithReturn = function(Target) {
 		var turnResult = "success";
 		if (!(this.isOccupied(Target[0], Target[1]))) {
-			this.putChip(this.Chip, Target[0], Target[1]);
+			this.putChip(this.Chip, Target[0], Target[1]); //Succesful chip.
 			
 			var line = this.theresLine();
 			var tie = this.theresTie();
 			if (line) {
-				turnResult = line;
+				turnResult = line; //End this game and declare a winner.
 			} else {
 				if (tie) {
-					turnResult = tie;
+					turnResult = tie; //End this game with no winner.
 				}
 			}
 			
 			if (line || tie) {
-				this.emptyBoard();
+				this.emptyBoard(); //Default board.
 			}
 			this.changeTurn();
 		} else {
-			console.log("Casilla ocupada.");
+			console.log("Casilla ocupada."); //Oh my... That square was already taken.
 			turnResult = "busy";
 		}
 		
 		if (this.fastGame) {
-			return turnResult;
+			return turnResult; //Just say if there was a line or not.
 		} else {
 			var row = parseInt(Target[0]) + 1;
 			var column = parseInt(Target[1]) + 1;
-			return ["row" + row, "column" + column, turnResult];
+			return ["row" + row, "column" + column, turnResult]; //Pronounce position and result.
 		}
-		
 	}
 	
+	//Useless right now for talkative game.
 	this.humanTurnFlow = function () {
 		console.log(this.showBoard());
 		var Target = this.decideHuman();
@@ -473,12 +502,14 @@ var Board = function (Chip) {
 		this.basicTurnFlow(Target);
 	}
 	
+	//First "AI" level. Pick a random empty square from a list.
 	this.randomIA = function () {
 		var empties = this.listEmptySquares();
-		var choice = parseInt(Math.random() * empties.length);
+		var choice = parseInt(Math.random() * empties.length); //Multiply random per possibilities. The old way.
 		return empties[choice];
 	}
 	
+	//Also deprecated. For the manual "Voice" button.
 	this.turnFlowWithVoice = function(sample) {
 		this.voiceReceiver.start();
 		var Target = this.recognizePosition(sample);
@@ -486,28 +517,30 @@ var Board = function (Chip) {
 		console.log(this.showBoard());
 	}
 
+	//The current core of voice play.
 	this.turnFlowWithVoiceAuto = function() {
-		var turnResult = "void";
+		var turnResult = "void"; //Default value. This variable will dictate voices played when the turn ends.
 		var anotherVoice = new webkitSpeechRecognition();
 		anotherVoice.lang = "es-ES";
+		
 		anotherVoice.onresult = function (event) {
 			if (event.results.length > 0) {
 				phrase1 = event.results[0][0].transcript;
 				Target1 = parentThis.recognizePosition(phrase1);
-				if (!(Target1[0] == -1 || Target1[1] == -1)) {
-					turnResult = parentThis.basicTurnFlowWithReturn(Target1);
-				} else {
-					var command = parentThis.compareCommand(phrase1, dictPlayMenu);
+				if (!(Target1[0] == -1 || Target1[1] == -1)) { //Both numbers are right?
+					turnResult = parentThis.basicTurnFlowWithReturn(Target1); //Process them. Put chip if possible.
+				} else { //They're not numbers!
+					var command = parentThis.compareCommand(phrase1, dictPlayMenu); //Compare pronounced word with command list.
 					if (command != "") {
-						turnResult = parentThis.commandPlayMenu(command);
-					} else {
+						turnResult = parentThis.commandPlayMenu(command); //Play and/or do it.
+					} else { //Command or position not recognized.
 						parentThis.failCount += 1;
 						turnResult = "unheard";
 					}
 					
 				}
 				console.log(parentThis.showBoard());
-				parentThis.micBusy = false;
+				parentThis.micBusy = false; //Free microphone and get ready for the next turn.
 			}
 		}
 		
@@ -520,21 +553,23 @@ var Board = function (Chip) {
 			}
 		}
 		
+		//Mandatory timed loop to work over Javascript asynchronous nature. Deal with it (until I know a cleaner way).
 		var waitPlease = setInterval(function () {
-			if (parentThis.micBusy == false) {
-				if (parentThis.failCount >= 4) {
+			if (parentThis.micBusy == false) { //Game started or microphone finished. Come.
+				if (parentThis.failCount >= 4) { //Too many recognition errors. Quit loop and, therefore, application.
 						parentThis.audioQueue([parentThis.Voice["not-working"]], 1);
 						clearInterval(waitPlease);
 				} else {
-					parentThis.micBusy = true;
-					var voiceQueue = [];
-					if (["Array", "object"].indexOf(typeof(turnResult)) > -1) {
+					parentThis.micBusy = true; //While true, speech rec won't start again.
+					var voiceQueue = []; //Later it will be filled with voice samples to play.
+					if (["Array", "object"].indexOf(typeof(turnResult)) > -1) { //More than one sample for turn result.
 						for (var i in turnResult) {
 							voiceQueue.push(parentThis.Voice[turnResult[i]]);
 						}
 					} else {
 						voiceQueue.push(parentThis.Voice[turnResult]);
-					}
+					} //Voice queue first refill.
+					
 					if (["vertical", "horizontal", "diagonal"].indexOf(turnResult[2]) > -1) {
 						var otherColor = parentThis.changeTurn(true)[1];
 						voiceQueue.push(parentThis.Voice[otherColor]);
@@ -542,10 +577,14 @@ var Board = function (Chip) {
 					}
 					if ("tie" == turnResult[2]) {
 						voiceQueue.push(parentThis.Voice["start"]);
-					}
-					voiceQueue.push(parentThis.Voice["turnfor"]);
+					} //Extra voice if game has ended.
+					
+					//Next turn for...
+					voiceQueue.push(parentThis.Voice["turnfor"]); 
 					voiceQueue.push(parentThis.Voice[parentThis.Color]);
 					console.log(voiceQueue);
+					
+					//Now play everything. Later listen to the mic, or get the AI working, to put the next chip.
 					parentThis.audioQueue(voiceQueue, 200, function() {
 						switch (parentThis.players[parentThis.Chip]) {
 							//case
@@ -567,11 +606,13 @@ var Board = function (Chip) {
 		
 	}
 	
+	//Future interactiveness.
 	this.mainMenu = function() {
 		var words = dictMainMenu;
 		//Mostrar descripción y opciones disponibles y esperar respuesta.
 	}
 	
+	//Another attempt to put everything in a FIFO queue. Still no good.
 	this.mainQueueProcess = function(queue) {
 		
 		var queueSeason = setInterval(function() {
@@ -613,6 +654,7 @@ var Board = function (Chip) {
 		}, 100);
 	}
 	
+	//To play visually, with alert windows.
 	this.startGame = function () {
 		this.emptyBoard();
 		while (1) {
@@ -620,6 +662,7 @@ var Board = function (Chip) {
 		}
 	}
 	
+	//Let the (real) game begin.
 	this.startGameWithVoice = function () {
 		if (this.microphoneWorks == false) {
 			this.checkMicrophone();
